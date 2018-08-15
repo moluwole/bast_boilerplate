38c7cc294128906e709538f437d83f683fc9134c
abiodun_solomon <habbeboynetwork@gmail.com>
2018-08-15 14:31:12 +0100

    Bast Assets

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,10 +1,4 @@
 let mix 	= require('bast-asset');
-// let elixir  = require('laravel-elixir');
-
 
 mix.js('public/assets/js/bast.js', 'public/static/js')
    .sass('public/assets/sass/app.scss', 'public/static/css');
-
-// elixir(function(mix) {
-//     mix.coffee('public/assets/js/app.coffee', 'public/static/js');
-// });
